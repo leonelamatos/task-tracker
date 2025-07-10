29cebef0fdedf7a1daeb55c42df79ba1375cefab
Leonel <leonel.amatos@gmail.com>
2025-07-10 17:57:03 -0400

    feat(util): init setup to connect to the appwrite services

---
+++ server/utils/appwriteInit.js
@@ -0,0 +1,18 @@
+import { Client, Databases, } from 'node-appwrite'
+
+const client = new Client()
+
+client.setEndpoint('https://nyc.cloud.appwrite.io/v1')
+    .setProject('686e9091002ccc1084b6')
+    .setKey(process.env.APPWRITE_API_KEY)
+
+
+
+const DATABASE_ID = process.env.APPWRITE_DATABASE_ID
+const COLLECTION_ID = process.env.APPWRITE_COLLECTION_ID
+
+
+const database = new Databases(client)
+
+
+export {DATABASE_ID,COLLECTION_ID,database}
