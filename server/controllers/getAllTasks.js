26afebbbaa47920bbe6708b0ac0e845d7ef6ab2c
Leonel <leonel.amatos@gmail.com>
2025-07-10 22:33:46 -0400

    feat: add logging to the get tasks endpoint

--- server/controllers/getAllTasks.js
+++ server/controllers/getAllTasks.js
@@ -1,13 +1,16 @@
 
 import { COLLECTION_ID, database, DATABASE_ID } from '../utils/appwriteInit.js'
 import { handleAsync } from '../utils/handleAsync.js'
+import { logger } from '../utils/logger.js'
 
 
 export const getAllTask = async (req, res) => {
 
     const [ error, data ] = await handleAsync(database.listDocuments(DATABASE_ID, COLLECTION_ID))
     
-    if(error) return rej
+    // if(error) return rej
     const tasks = data.documents
+
+    logger.info(`data accessed`)
     return res.json(tasks)
 }
