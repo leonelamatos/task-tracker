895a81ed7a6fe13af1f10917cc4e924e96c42b2e
Leonel <leonel.amatos@gmail.com>
2025-07-12 20:19:40 -0400

    configure backend integration

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react-swc'
 
+
 // https://vite.dev/config/
-export default defineConfig({
- 
-  plugins: [react(),tsconfigPaths(),],
+export default defineConfig(({ mode }) => {
+
+  const env = loadEnv(mode, process.cwd());
+
+ return {
+  plugins: [ react(), tsconfigPaths(), ],
+  base: '/track-tracker',
+  server: {
+    cors: { origin: 'http://localhost:3000' },
+   
+    proxy: {
+      "/api/": {
+        target: env.VITE_API_BASE_URL,
+        changeOrigin: true,
+        
+      }
+    }
+  },
+   build:{manifest: true},}
 })
--- server/server.js
+++ server/server.js
@@ -2,12 +2,8 @@
 import express from 'express' 
 import {logger} from './utils/logger.js'
 import { errorHandler } from './middlewares/errorHandler.js'
-import { deleteTask, getAllTask, getSingleTask, postTasks, updateTask } from './controllers/tasksControllers.js'
+import { deleteTask, getAllTask, getTaskById, postTasks, updateTask } from './controllers/tasksControllers.js'
 import cors from 'cors'
-import { postCreateUserAccount, postLoginUser } from './controllers/authController.js'
-
-
-// config()
 
 const PORT = process.env.SERVER_PORT
 
@@ -18,14 +14,11 @@ app.use(express.json())
 app.use(errorHandler)
 app.use(cors({origin:'http://localhost:5173'}))
 
-app.get('/tasks', getAllTask)
-app.get('/tasks/:id', getSingleTask)
-app.post('/tasks', postTasks)
-app.put('/tasks/:id', updateTask)
-app.delete('/tasks/:$id', deleteTask)
-app.post('/auth/login', postLoginUser)
-app.post('/auth/singup', postCreateUserAccount)
-
+app.get('/api/tasks', getAllTask)
+app.get('/api/tasks/:id', getTaskById)
+app.post('/api/tasks', postTasks)
+app.put('/api/tasks/:id', updateTask)
+app.delete('/api/tasks/:id', deleteTask)
 
 app.listen(PORT, () => {
     logger.info(`server started on port ${PORT}`)
